ada7d40366c813e126c65279b9d76df9b8dd2975
Giselle Brown <browng5287@my.sduhsd.net>
2021-01-12 17:35:58 +0000

    continued C4

--- areaofcircle2.c
+++ areaofcircle2.c
@@ -12,20 +12,49 @@ float areaOfCircle(float radius)
 
 int main(int argc, char* argv[]) 
 {
-  int arg = 0; 
+  float start;
+  float end;
+  char buffer[256];
+
   if (argc < 2)
   {
-  
+    printf("What do you want the starting radius to be\n");
+    while (1)
+    {
+      fgets(buffer, 256, stdin);
+      if (sscanf(buffer, "%f", &start) == 1) break;
+      printf("Not a valid float - try again!\n");
+    }
+
+   printf("What do you want the ending raduis to be\n");
+   while (1)
+   {
+     fgets(buffer, 256, stdin);
+     if (sscanf(buffer, "%f", &end) == 1) break;
+     printf("Not a valid float - try again!\n");
+   }
+   printf("calculating area of circle starting at %f, and ending at %f\n", start, end);
+   float reps = end - start; 
+   for (int i = 0; i < reps; i++)
+   {
+    float calculation  = areaOfCircle(start);
+    printf("area for radius %f is %f\n", start, calculation);
+    start++;
+   }
+  }
+  else
+  {
+   
+
+  }
   // the two variables which control the number of times areaOfCircle is called
   // in this case 5.2, 6.2, 7.2
-  float start = 5.2;
-  int reps = 3;
 
-  printf("calculating area of circle starting at %f, and ending at %f\n", start, start+reps-1);
+  printf("calculating area of circle starting at %f, and ending at %f\n", start, end);
   
   // add your code below to call areaOfCircle function with values between
   // start and end
-
+  float reps = end - start;
   for (int i = 0; i < reps; i++)
   {
     float calculation  = areaOfCircle(start);
