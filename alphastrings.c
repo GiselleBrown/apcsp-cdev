1a5a9d9b67ba766bc1946309705f519f141f1a2d
Giselle Brown <browng5287@my.sduhsd.net>
2021-01-12 16:37:29 +0000

    not compleated C4 projects

---
+++ alphastrings.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  // string created using for loop
+  char stringOne[27];
+  for (int i = 0; i < 26; i++)
+  {
+    stringOne[i] = 'a' + i;
+  }
+  // string created using constant initializer
+  char stringTwo[] = "abcdefghijklmnopqrstuvwxyz";
+
+  // Comparing the string
+  if (strcmp(stringOne, stringTwo) == 0)
+  {
+    printf("stringOne and stringTwo are the same\n");
+  }
+  else
+  {
+    printf("not working\n");
+  }
+  //Turing one into uppercase
+  for (int i = 0; i < 26; i++)
+  {
+    stringTwo[i] -= 32;
+  }
+
+  // Comparing to see if different
+  if (strcmp(stringOne, stringTwo) != 0)
+  {
+    printf("they no longer equal each other\n");
+  }
+
+  // Joint upper and lower case
+  char stringThree[54];
+  strcpy(stringThree, stringTwo);
+  strcpy(stringThree, stringOne);
+
+  // Printing
+  printf("String One: %s\n String Two: %s\n String Three: %s\n", stringOne, stringTwo, stringThree);
+
+}
